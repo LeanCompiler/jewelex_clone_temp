c6be0ab0cbbfd891e885f2e3bb9fcd5a8f3a4fee
LeanCompiler <leancompiler@gmail.com>
2025-05-26 18:48:04 +0530

    update sqlite db dir

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./data/urls.db",
+  storage: "/home/deploy/lead_data_collection/data/db/urls.db",
   logging: false, // turn on for debugging
 });
 
